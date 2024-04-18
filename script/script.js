8375c83c82db381d9f7877ef0c2f959ec210232b
Ile00 <162448345+Ile00@users.noreply.github.com>
2024-04-18 11:23:14 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,1 +1,7 @@
-function 
+function doLogin()  {
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+if(username == "" || password == "") {
+alert("Username e password sono obbligatori");   
+}  
+                                    
