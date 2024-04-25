26410838789fd237666d9b9bb458792989ffd3fc
Ile00 <162448345+Ile00@users.noreply.github.com>
2024-04-25 16:19:00 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -3,5 +3,22 @@ var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
 if(username == "" || password == "") {
 alert("Username e password sono obbligatori");   
-}  
-                                    
+}else{
+ // chiamiamo il server per effettuare la verifica
+ fetch('https://www.cacciaapi.altervista.org/login.php/loginIpad', {
+  method: 'POST', // o 'GET'
+  headers: {
+   'Content-Type':  'application/json',
+},
+body: JSON.stringify({
+  "username": username,
+  "password": password
+ })
+})
+ .then (response =› response.json()) // converte la risposta in JSON
+ .then (data => console.log (data)) // stampa i dati
+ .catch(error) => {
+  console.error('Errore:',error);
+ }):
+}
+}                                   
